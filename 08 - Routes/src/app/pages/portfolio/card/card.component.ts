aa2b761a73412fdf3f52b919236160e6bbe6767e
felipefalkiner <felipe.magosso@gmail.com>
2023-09-27 11:00:30 -0300

    routes course: get query params

--- 08 - Routes/src/app/pages/portfolio/card/card.component.ts
+++ 08 - Routes/src/app/pages/portfolio/card/card.component.ts
@@ -9,6 +9,12 @@ import { ActivatedRoute } from '@angular/router';
 export class CardComponent implements OnInit {
 
   constructor(private routerParams: ActivatedRoute) {
+    // Query Params
+    // http://localhost:4200/portfolio/5?{name=Falk&notSecretToken=1234}
+    this.routerParams.queryParams.subscribe( res => console.log(res))
+
+    // Route Params
+    // http://localhost:4200/portfolio/{5}
     this.routerParams.params.subscribe( res => console.log(res))
    }
 
