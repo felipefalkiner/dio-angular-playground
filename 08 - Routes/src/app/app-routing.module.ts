954ad774d593b18109e6aa56cba440310932a76f
felipefalkiner <felipe.magosso@gmail.com>
2023-09-27 10:50:40 -0300

    routes course: activeRouter or routerLinkActive

--- 08 - Routes/src/app/app-routing.module.ts
+++ 08 - Routes/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { CardComponent } from './pages/portfolio/card/card.component';
 
 const routes: Routes = [
   {path:"", component:TitleComponent, pathMatch:'full'},
-  {path:"portfolio", component:CardComponent, pathMatch:'prefix'},
+  {path:"portfolio", component:CardComponent},
   {path:"**", redirectTo:''}, // you can use this for 404 page for example
 ];
 
