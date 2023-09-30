9804fe48d09f39eca036e41e549a05686656942b
felipefalkiner <felipe.magosso@gmail.com>
2023-09-30 16:14:05 -0300

    services course: edites card component ts file

--- 09 - Services/src/app/components/card/card.component.ts
+++ 09 - Services/src/app/components/card/card.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
+  name:string = 'Bulbassaur';
+  attributesTypes:string[] = ['Fire', 'Dragon']
+  picture:string = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png'
+
+
   constructor() { }
 
   ngOnInit(): void {
